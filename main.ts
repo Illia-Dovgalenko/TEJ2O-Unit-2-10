fb70183afc63e1a4de33af7a80d291660b4cf485
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-25 05:12:44 +0200

    uses neopixels on RobotBit

--- main.ts
+++ main.ts
@@ -1,8 +1,59 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: March 2026
+ * This program uses neopixels on RobotBit
 */
 
-basic.showString('Hello, World!')
+let lightlevel: number = 0
+let neopixelStrip: neopixel.Strip = null
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+
+    // light level
+    lightlevel = input.lightLevel()
+    basic.showNumber(lightlevel)
+    // 0 neopixels
+    if (lightlevel <= 51) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+    }
+    // 1 neopixel
+    if (lightlevel > 52) {
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+        neopixelStrip.show()
+    }
+    // 2 neopixels
+    if (lightlevel > 104) {
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+    }
+    // 3 neopixels
+    if (lightlevel > 156) {
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+    }
+    // 4 neopixels
+    if (lightlevel > 208) {
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.show()
+    }
+})
